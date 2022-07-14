48d14c212abb1721841fa32cd63b0786ba689ab3
Radhakrishnan Meiappan <rmeiappan@rocketsoftware.com>
2022-07-14 09:21:32 +0530

    changes

--- 01-helloworld/src/helloworld.java
+++ 01-helloworld/src/helloworld.java
@@ -3,6 +3,11 @@ public class helloworld {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		greet("John");
+	}
+	
+	public static void greet(String name) {
+		System.out.println("Hello " + name);
 	}
 
 }
