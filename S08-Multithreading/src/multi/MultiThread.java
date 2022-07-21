a971184858194e5f987e2b0976a4028d25096d87
Radhakrishnan Meiappan <rmeiappan@rocketsoftware.com>
2022-07-21 12:23:01 +0530

    changes

--- S08-Multithreading/src/multi/MultiThread.java
+++ S08-Multithreading/src/multi/MultiThread.java
@@ -2,19 +2,25 @@ package multi;
 
 public class MultiThread extends Thread {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		MultiThread mt = new MultiThread();
 		mt.start();
 		
 		for (int j=1; j<=200; j++) {
 			System.out.print("j: " + j + "\t");
+			Thread.sleep(500);
 		}
 	}
 
 	@Override
 	public void run() {
 		for (int i=1; i<=200; i++) {
-			System.out.print("i: " + i + "\t");
+			System.out.println("i: " + i + "\t");
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
