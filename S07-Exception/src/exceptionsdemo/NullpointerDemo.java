e2e048e857a6235c071c9595b2ade6788a1dc947
Radhakrishnan Meiappan <rmeiappan@rocketsoftware.com>
2022-07-21 09:43:15 +0530

    exceptions

---
+++ S07-Exception/src/exceptionsdemo/NullpointerDemo.java
@@ -0,0 +1,12 @@
+package exceptionsdemo;
+
+public class NullpointerDemo {
+	
+	public static void main(String[] args) {
+		
+		String s = null;
+		System.out.println("String Length: " + s.length());
+		
+	}
+
+}
---
+++ S07-Exception/src/exceptionsdemo/StringParser.java
@@ -0,0 +1,13 @@
+package exceptionsdemo;
+
+public class StringParser {
+
+	public static void main(String[] args) {
+		
+		String s = "abcd";
+		int a = Integer.parseInt(s);
+		System.out.println("Result: " + a);
+		
+	}
+	
+}
