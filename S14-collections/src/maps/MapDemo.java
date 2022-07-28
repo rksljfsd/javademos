7ab1f67fcd0c6a4bc127acdcc4be747a31c37c5b
Radhakrishnan Meiappan <rmeiappan@rocketsoftware.com>
2022-07-28 11:59:36 +0530

    changes

--- S14-collections/src/maps/MapDemo.java
+++ S14-collections/src/maps/MapDemo.java
@@ -1,15 +1,15 @@
 package maps;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class MapDemo {
 	
 	static Map<String, String> users;
 
 	public static void main(String[] args) {
-		
-		
 		
 		Map<String, Integer> scores = new HashMap<String, Integer>();
 		
@@ -25,7 +25,7 @@ public class MapDemo {
 		users = new HashMap<String, String>();
 		users.put("RK", "rk123");
 		users.put("Ashok", "9873");
-		users.put("Saga", "kd73");
+		users.put("Sagar", "kd73");
 		System.out.println(users);
 		
 		System.out.println("Password for user RK = " + users.get("RK"));
@@ -33,6 +33,15 @@ public class MapDemo {
 		System.out.println("Is User Authenticated: " + authenticate("RK", "rk123"));
 		System.out.println("Is User Authenticated: " + authenticate("RK", "rk1234"));
 		
+		Set<String> keys = users.keySet();
+		System.out.println(keys);
+		
+		Collection<String> values = users.values();
+		System.out.println(values);
+		
+		for (String key : keys) {
+			System.out.println("Key: " + key + ", Value: " + users.get(key));
+		}
 		
 	}
 	
